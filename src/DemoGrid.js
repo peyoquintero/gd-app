a12a4b72b4d97338b222b5b814fba413ea8e921b
Pedro Quintero <pedro.quintero@blackline.com>
2023-09-02 13:19:26 -0400

    KindaWorking

--- src/DemoGrid.js
+++ src/DemoGrid.js
@@ -1,12 +1,12 @@
-import React, { Component, useState  } from 'react';
+import React, { useState  } from 'react';
 
-function filteredGData(props) {
+function filteredGData(props,gData) {
     let filterKey = props.filterKey && props.filterKey.toLowerCase()
     let sortOrders= props.columns.reduce((o, key) => ((o[key] = 1), o), {})
     const order = sortOrders[props.sortKey] || 1
-    let data = props.data.filter(w=>(props.excludeChar==="") || (w.Codigo.indexOf(props.excludeChar) === -1))
+    let filteredData = gData.filter(w=>(props.excludeChar==="") || (w.Codigo.indexOf(props.excludeChar) === -1))
     if (filterKey) {
-      data = data.filter((row) => {
+        filteredData = filteredData.filter((row) => {
         return Object.keys(row).filter(w=>w!==props.excludeFilter).some((key) => {
           if(!filterKey.includes(";"))
               return String(row[key]).toLowerCase().indexOf(filterKey) > -1
@@ -21,71 +21,74 @@ function filteredGData(props) {
       })
     }
     if (props.sortKey) {
-      data = data.slice().sort((a, b) => {
+        filteredData = filteredData.slice().sort((a, b) => {
         a = a[props.sortKey]
         b = b[props.sortKey]
         return (a === b ? 0 : a > b ? 1 : -1) * order
       })
     }
-    return data
+    return filteredData
   }
 
-  function headerKeyMapping(props,headerKey)
-  {
-    var index = props.headers.indexOf(headerKey); 
-    return props.columns[index];
-  }
-
-  function colwidth(key)
-  {
-    var index = this.props.headers.indexOf(key); 
-    return `column-width:${this.props.headerwidthpct[index]}%`;
-  }
-
-  function columnWidth(props,key)
-  {
-    var index = props.headers.indexOf(key); 
-    var w = props.headerwidthpct[index];
-    const style = {
-        width: w 
-    };
-
-    return style;
-}
-
-  function sortBy(filteredData,key) {
-    //Todo: data returned??
-    filteredData[key] = filteredData[key] * -1
-  }
-
-  function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  }
   
-class DemoGrid extends Component { 
-    constructor()
-    {
-        const [data, setData] = useState(props.data); 
-        const [sortKey, setSortKey] = useState(props.sortKey); 
-    }
-    render(){ 
-          let filteredData = filteredGData(this.props)
-          let myprops = this.props
+export const  DemoGrid = (props) =>  { 
+//        const [data, setData] = useState(props.data); 
+//        const [sortKey, setSortKey] = useState(props.sortKey); 
+
+        function headerKeyMapping(headers,columns,headerKey)
+        {
+          var index = headers.indexOf(headerKey); 
+          return columns[index];
+        }
+      
+        function colwidth(key)
+        {
+          var index = this.props.headers.indexOf(key); 
+          return `column-width:${this.props.headerwidthpct[index]}%`;
+        }
+      
+        function columnWidth(props,key)
+        {
+          var index = props.headers.indexOf(key); 
+          var w = props.headerwidthpct[index];
+          const style = {
+              width: w 
+          };
+      
+          return style;
+      }
+      
+        function sortBy(filteredData,key) {
+          //Todo: data returned??
+          filteredData[key] = filteredData[key] * -1
+        }
+      
+        function capitalize(str) {
+          return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+      
+          let filteredData = filteredGData(props,props.data);
+          //setData(filteredData);
           if (filteredData.length)
           {
             return( 
             <table>
             <thead>
-                {myprops.headers.map((key)=>{return (
-                <th style={columnWidth(myprops,key)} 
-                onClick={setData(sortBy(filteredData,headerKeyMapping(key)))}
-                className={myprops.sortKey === key? 'active':''}> 
+            <tr>
+                {props.headers.map((key)=>{return (
+                <th style={columnWidth(props,key)} 
+//                onClick={setData(sortBy(filteredData,headerKeyMapping(props.headers,props.columns,key)))}
+                onClick={sortBy(filteredData,headerKeyMapping(props.headers,props.columns,key))}
+                className={props.sortKey === key? 'active':''}
+                key={key}> 
                 {capitalize(key)}
                 </th>)})}
+                </tr>
             </thead>
             <tbody>
-            {filteredData.map((entry)=>{return (<tr>
-                {myprops.columns.map((key)=>entry[key])}
+            {filteredData.map((entry)=>{
+                return (<tr key={entry.Codigo}>
+                {props.columns.map((mykey)=>{return(<td key={mykey}>{entry[mykey]}</td>)})}
             </tr>)})}
             </tbody>
           </table>)
@@ -99,6 +102,5 @@ class DemoGrid extends Component {
           )
             }
         }
-}
 
-export default DemoGrid    
+        export default DemoGrid;
