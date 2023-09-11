0e7dd0452fc830dcab94a899e24b7ed54c89a8cb
Pedro Quintero <pedro.quintero@blackline.com>
2023-09-10 22:36:22 -0400

    Sort kind of working

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,12 +6,24 @@ import { useSortableTable } from "./useSortableTable";
 const Table = ({ caption, data, columns }) => {
 // ToDo; Fix sorting
 const [tableData, setTableData] = useState([]);
+const [sortOrder,setSortOrder] = useState({accessor:columns[0].accessor,sortOrder:'down'})
 useEffect(() => { setTableData(data) }, [data]);
+useEffect(() => { 
+  let sortKey = sortOrder.accessor;
+  let data  = tableData.slice().sort((a, b) => {
+      a = a[sortKey]
+      b = b[sortKey]
+      return (a === b ? 0 : a > b ? 1 : -1) 
+    });
+
+  setTableData(data);
+}, [sortOrder]);
+
 return (
     <>
       <table className="table">
-        <TableHead {...{ columns }} />
-        <TableBody {...{ tableData, columns }} />
+        <TableHead {...{ columns, setSortOrder, sortOrder}} />
+        <TableBody {...{ tableData, columns  }} />
       </table>
     </>
   );
--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useCallback } from "react";
 
-const TableHead = ({ columns }) => {
+const TableHead = ({ columns,setSortOrder,sortOrder }) => {
+    const onclickHandler = (e) => 
+    {
+        setSortOrder({accessor:e.currentTarget.getAttribute("data-id"),
+                      sortOrder : sortOrder.sortOrder === 'down' ? 'up' : 'down'
+        });
+        console.log(e.currentTarget.getAttribute("data-id")) 
+    }
     return (
      <thead>
       <tr>
        {columns.map(({ label, accessor, width }) => {
         return (
-         <th key={accessor} style={{width:width}} >
+         <th key={accessor} style={{width:width}}  onClick={onclickHandler} data-id={accessor}>
           {label}
          </th>
         );
