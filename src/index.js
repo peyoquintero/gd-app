2534f8b776ec28adbcd98c594c9fcc75f9521ca1
rbquintero <raquelbquintero@gmail.com>
2023-10-05 15:08:56 -0400

    Rework PWA

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import {  App,  HisPesajes,  HisCodigos, GananciasDiarias,AyudaGD} from "./App";
 import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
 import { register as registerServiceWorker } from './serviceWorkerRegistration';
 
-registerServiceWorker();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -20,6 +18,8 @@ root.render(
     </Routes>
   </BrowserRouter> 
    </React.StrictMode>
-   
 );
+
+registerServiceWorker();
+
 
