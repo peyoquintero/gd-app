148b1af205491ed3865d59356ad3761fb9006712
rbquintero <raquelbquintero@gmail.com>
2023-10-05 06:01:33 -0400

    serviceWroker call moved

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {  App,  HisPesajes,  HisCodigos, GananciasDiarias,AyudaGD} from "./App";
 import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
 import { register as registerServiceWorker } from './serviceWorkerRegistration';
 
+registerServiceWorker();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -21,4 +23,3 @@ root.render(
    
 );
 
-registerServiceWorker();
