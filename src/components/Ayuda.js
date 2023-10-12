c1468054d5e960bd5aa51e7158dd7ae19d7bdec4
Pedro Quintero <pedro.quintero@blackline.com>
2023-10-12 15:46:42 -0400

    Vendidos en limbo

--- src/components/Ayuda.js
+++ src/components/Ayuda.js
@@ -9,51 +9,46 @@ const Ayuda = (props) => {
     const [filtros,setFiltros] = useState({filtroDups:false,filtroMuertos:false});
     const [gridDups,setGridDups] = useState([]);
     const [gridMuertes,setGridMuertes] = useState([]);
+    const [selectedOption, setSelectedOption] = useState("");
 
-       useEffect(()=>{
-        let allPesajes =  JSON.parse(localStorage.getItem("spreadsheetData"));
-        if (allPesajes?.length)
-        {
-          setGridDups(duplicates(allPesajes)) ;
-          let muertes = allPesajes.filter(w=>w.Operacion.toUpperCase().trim()==='MUERTE')
-                        .sort(function(a,b){return new Date(a.Fecha) - new Date(b.Fecha);})
-          setGridMuertes(muertes);
-        }
-      },[]);
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
 
-      const columnsMuertes = [
-        { label: "Codigo", accessor: "Codigo",width:"30%" },
-        { label: "Fecha", accessor: "Fecha",width:"40%" },
-        { label: "Marca", accessor: "Marca",width:"30%" },
-       ];
+    const handleCheckboxChange = (event) => {
+      const { name } = event.target;
+      setFiltros({
+        ...filtros,
+        [name]: event.target.checked,
+      });
+    };
 
-      const handleCheckboxChange = (event) => {
-        const { name } = event.target;
-        setFiltros({
-          ...filtros,
-          [name]: event.target.checked,
-        });
-      };
+   useEffect(()=>{
+    let allPesajes =  JSON.parse(localStorage.getItem("spreadsheetData"));
+    if (allPesajes?.length)
+    {
+      setGridDups(duplicates(allPesajes)) ;
+      let muertes = allPesajes.filter(w=>w.Operacion.toUpperCase().trim()==='MUERTE')
+                    .sort(function(a,b){return new Date(a.Fecha) - new Date(b.Fecha);})
+      setGridMuertes(muertes);
+    }
+    },[]);
+
+   const columnsMuertes = [
+    { label: "Codigo", accessor: "Codigo", width:"30%" },
+    { label: "Fecha", accessor: "Fecha", width:"40%" },
+    { label: "Marca", accessor: "Marca", width:"30%" }
+    ];
 
     return (
         <div >
-            <section className="title">
-                <label className="ayudaLabel" >Inconsistencias
-                    <input  type="checkbox" id="checkboxDup" name= "filtroDups" onChange={handleCheckboxChange} />
-                </label>
+            <section className="radio-container" onChange={handleChange}>
+              <input type="radio" name="details1" value="optionInconsistencias" checked={selectedOption === "optionInconsistencias"} />Inconsistencias
+              <input type="radio" name="details2" value="optionRevisionCodigos" checked={selectedOption === "optionRevisionCodigos"} /> Revision Codigos
             </section>
             <section style={{background:'rgb(249, 249, 249)'}}>
-               {(filtros?.filtroDups &&gridDups?.length>0 ) ? <IntegerMatrix nColumns={5} integers={gridDups}></IntegerMatrix> : null}
-            </section>
-            <section className="title">
-                <label className="ayudaLabel" >Codigos -  Revision
-                    <input  type="checkbox" id="checkboxCodigos" name= "filtroCodigos" onChange={handleCheckboxChange} />
-                </label>
-            </section>
-            <section>
-            {filtros?.filtroCodigos ? 
-              <Codigos/> :
-              null}
+               {((selectedOption === "optionInconsistencias") && gridDups?.length>0 ) ? <IntegerMatrix nColumns={5} integers={gridDups}></IntegerMatrix> : null}
+               {(selectedOption === "optionRevisionCodigos")?  <Codigos/> : null}
             </section>
             <section className="title" >
                 <label className="ayudaLabel" >Muertes
--- src/components/Inventario.js
+++ src/components/Inventario.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Table from "./Table";
+import {getInventario,groupByFechaOperacion} from "./HelperInventario";
 
 const Inventario = () => {
   const columns = [
@@ -9,7 +10,6 @@ const Inventario = () => {
     { label: "Total", accessor: "Total",width:"15%" },
     { label: "Vendidos", accessor: "Vendidos",width:"15%" },
    ];
-
 
    const [selectedOption, setSelectedOption] = useState("movimientos");
 
@@ -29,68 +29,6 @@ const Inventario = () => {
     { label: "Ultimo Peso", accessor: "PesoFinal",width:"10%" },
    ];
 
-   function getInventario(data) {
-    data.sort((a, b) => a.Codigo.localeCompare(b.Codigo));
-    data.sort(function(a,b){return new Date(a.Fecha) - new Date(b.Fecha); })
-    const groupedData = data.reduce((acc, item) => {
-      const key = `${item.Codigo}`;
-      if (!acc[key]) {
-        acc[key] = {
-          Codigo: item.Codigo,
-          Marca: item.Marca,
-          Pesajes: [],
-        };
-      }
-      acc[key].Pesajes.push(item)
-      return acc;
-    }, {});
-
-    var result = Object.values(groupedData);
-
-    let codigosVendidos = data.filter(w=>['VENTA','MUERTE'].includes(w?.Operacion?.toUpperCase())).map(x=>x.Codigo);
-    result = result.filter(x => !codigosVendidos.includes(x.Codigo));
-
-    result = result.map(w=> {return {
-      Codigo: w.Codigo,
-      Marca: w.Marca,
-      FechaCompra: w.Pesajes[0]?.Fecha,      
-      PesoInicial: w.Pesajes[0]?.Peso,
-      FechaUltimoControl: w.Pesajes[w.Pesajes.length-1]?.Fecha,
-      PesoFinal: w.Pesajes[w.Pesajes.length-1]?.Peso
-                           }});
-    return result;
-  }
-
-   function intersectCount (comprados,vendidos){
-    return comprados.filter(w=>vendidos.includes(w)).length;
-   }
-
-   function groupByFechaOperacion(data) {
-    const groupedData = data.reduce((acc, item) => {
-      const key = `${item.Fecha}-${item.Operacion}-${item.Marca}`;
-      if (!acc[key]) {
-        acc[key] = {
-          Fecha: item.Fecha,
-          Operacion: item.Operacion,
-          Marca: item.Marca,
-          Total: 0,
-          Codigos: [],
-          Vendidos: 0
-        };
-      }
-      acc[key].Total++;
-      acc[key].Codigos.push(item.Codigo)
-      return acc;
-    }, {});
-
-    var result = Object.values(groupedData);
-    let codigosVendidos = data.filter(w=>w?.Operacion?.toUpperCase()==='VENTA').map(x=>x.Codigo);
-
-    result = result.map(w=>{w.Vendidos= w?.Operacion?.toUpperCase()!=='COMPRA'? 0 : intersectCount(w.Codigos??[],codigosVendidos??[]);
-                            return w;    
-                           });
-    return result;
-  }
 
    useEffect(()=>{
     let allPesajes =  JSON.parse(localStorage.getItem("spreadsheetData"));
